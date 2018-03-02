99fcb33f59152f44ccee2909b234140273ee202b
Weihan Kong <kongweihan@gmail.com>
2018-03-01 22:12:18 -0500

    Homework 9: Barriers
    Use case of condition variables, the round only proceeds when all
    threads reach the function barrier().

--- barrier.c
+++ barrier.c
@@ -27,7 +27,16 @@ barrier_init(void)
 static void
 barrier()
 {
-  bstate.round++;
+  pthread_mutex_lock(&bstate.barrier_mutex);
+  bstate.nthread++;
+  if (bstate.nthread == nthread) {
+    bstate.round++;
+    bstate.nthread = 0;
+    pthread_cond_broadcast(&bstate.barrier_cond);
+  } else {
+    pthread_cond_wait(&bstate.barrier_cond, &bstate.barrier_mutex);
+  }
+  pthread_mutex_unlock(&bstate.barrier_mutex);
 }
 
 static void *
